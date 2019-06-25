97e01ea64ea7828606a074691f1d9bfca9485fc8
Sotatek-DucPham <duc.pham@sotatek.com>
2019-06-25 10:32:15 +0700

    sample tx

--- btc/mine.js
+++ btc/mine.js
@@ -1,18 +1,18 @@
-var http = require("http");
+const http = require("http");
+
+const options = {
+  method: "POST",
+  hostname: "127.0.0.1",
+  port: "8332",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: "Basic YWRtaW46MQ==",
+    "cache-control": "no-cache",
+    "Postman-Token": "9fb49641-c334-4afa-993a-19a203d97eb7"
+  }
+};
 
 function mine() {
-  var options = {
-    method: "POST",
-    hostname: "127.0.0.1",
-    port: "8332",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Basic YWRtaW46MQ==",
-      "cache-control": "no-cache",
-      "Postman-Token": "9fb49641-c334-4afa-993a-19a203d97eb7"
-    }
-  };
-
   var req = http.request(options, function(res) {
     var chunks = [];
 
@@ -32,4 +32,30 @@ function mine() {
   req.end();
 }
 
+function tx() {
+  var req = http.request(options, function(res) {
+    var chunks = [];
+
+    res.on("data", function(chunk) {
+      chunks.push(chunk);
+    });
+
+    res.on("end", function() {
+      var body = Buffer.concat(chunks);
+      console.log(body.toString());
+    });
+  });
+
+  req.write(
+    JSON.stringify({
+      jsonrpc: 2,
+      id: 1,
+      method: "sendtoaddress",
+      params: ["mghKRjdqkLWppwEvnQAfvx8GbUD7TDuLbo", 0.001]
+    })
+  );
+  req.end();
+}
+
 setInterval(() => mine(), 15000);
+setInterval(() => tx(), 500);
